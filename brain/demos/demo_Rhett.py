19fd15204eb727e9b3b6746787036df84aa1bd1d
Andrew personal laptop <sheare.andrew18@svvsd.org>
2016-06-04 17:27:44 -0600

    rhett has a demo now
    yay

---
+++ brain/demos/demo_Rhett.py
@@ -0,0 +1,39 @@
+import serial
+import subprocess
+import base
+import time
+import math
+
+demo_do = base.MoveCtrl()
+
+class DemoList:
+    def demoAction(self):
+        #### Play media file
+        subprocess.call("mplayer -vo fbdev2:/dev/fb1 -x 240 -y 320 -framedrop -quiet -input file=/home/pi/fifos /home/pi/BiBli/brain/demos/bibli_demo_Rhett.mp4 < /dev/null && sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/pi/BiBli/media/image/bg2.jpg &", shell=True)
+        time.sleep(2)
+        #### BiBli motion
+        demo_do.move('f')
+        time.sleep(2)
+        demo_do.move('s')
+        time.sleep(2)
+        demo_do.move('f')
+        time.sleep(1)
+        demo_do.move('s')
+        time.sleep(2)
+        demo_do.move('b')
+        time.sleep(1)
+        demo_do.move('s')
+        time.sleep(2)
+        demo_do.move('b')
+        time.sleep(2)
+        demo_do.move('s')
+        time.sleep(1)
+        demo_do.move('l')
+        time.sleep(2)
+        demo_do.move('r')
+        time.sleep(1)
+        demo_do.move('l')
+        time.sleep(2)
+        demo_do.move('r')
+        time.sleep(2)
+        demo_do.move('s')
